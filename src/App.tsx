89f6cce89c7c6869ef20e5bb4b24c58af8835d6d
PureGero <puregero@gmail.com>
2021-10-01 19:17:15 +1000

    Replace App.css with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${appLogoSpin} infinite 20s linear;
+  }
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
 
 interface AppProps {
   name: string;
@@ -9,22 +46,21 @@ interface AppProps {
 class App extends React.Component<AppProps> {
   render() {  
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <AppContainer>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
           <p>
             Edit <code>{this.props.name}</code> and save to reload!
           </p>
-          <a
-            className="App-link"
+          <AppLink
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
-      </div>
+          </AppLink>
+        </AppHeader>
+      </AppContainer>
     );
   }
 }
